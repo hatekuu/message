c93e16c4c799cebaeaa9d923d4e512c7d2f7339a
hatekuu <hatekuu@gmail.com>
2024-06-02 15:00:18 +0700

    Updates

---
+++ static/js/63.e7cde56d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknt=self.webpackChunknt||[]).push([[63],{63:(s,e,h)=>{h.r(e),h.d(e,{default:()=>t});h(43);var n=h(579);const t=()=>(0,n.jsx)("div",{children:"This is home"})}}]);
+//# sourceMappingURL=63.e7cde56d.chunk.js.map
